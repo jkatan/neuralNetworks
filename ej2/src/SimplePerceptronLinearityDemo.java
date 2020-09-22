4b3407967023561dbd0a728b44e0d590046b3664
npaganini <npaganin@itba.edu.ar>
2020-09-22 01:34:23 -0300

    Initial commit for ej2.

---
+++ ej2/src/SimplePerceptronLinearityDemo.java
@@ -0,0 +1,13 @@
+public class SimplePerceptronLinearityDemo {
+    public static void main(String[] args) {
+        // do stuff
+    }
+
+    public static void SimpleLinearPerceptron() {
+        // do linear stuff
+    }
+
+    public static void SimpleNonLinearPerceptron() {
+        // do non-linear stuff
+    }
+}
